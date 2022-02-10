dd25b9208186deb3d3b71fed3360a9705ecca5f7
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2022-02-10 20:58:35 +0000

    Fix include guard on utils.hpp

--- src/include/utils.hpp
+++ src/include/utils.hpp
@@ -1,9 +1,14 @@
+#ifndef _HUGOS_UTILS_H
+#define _HUGOS_UTILS_H
+
 #include <stdint.h>
 
-template<class T> void align_up(T* &ptr, uintptr_t alignment) {
-	ptr = reinterpret_cast<T*>((reinterpret_cast<uintptr_t>(ptr) + alignment - 1) & ~(alignment - 1));
+template<class T> T* align_up(T* ptr, uintptr_t alignment) {
+	return reinterpret_cast<T*>((reinterpret_cast<uintptr_t>(ptr) + alignment - 1) & ~(alignment - 1));
 }
 
-template<class T> void add_bytes(T* &ptr, uintptr_t offset) {
-	ptr = reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) + offset);
+template<class T> T* add_bytes(T* ptr, uintptr_t offset) {
+	return reinterpret_cast<T*>(reinterpret_cast<uintptr_t>(ptr) + offset);
 }
+
+#endif
