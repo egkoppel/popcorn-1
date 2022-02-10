3d9edba7a9e4d2627058e4611ddbb56168be8863
Oliver Hiorns <oshiorns1@gmail.com>
2022-02-10 19:26:26 +0000

    now check multiboot magic
    whereas before we simply printed it

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,5 +7,9 @@
 #include <termcolor.h>
 
 extern "C" void kmain(uint32_t multiboot_magic, uint32_t multiboot_addr) {
-	kprintf("[" TERMCOLOR_CYAN "INFO" TERMCOLOR_RESET "] Mulitboot magic: 0x%x\n", multiboot_magic);
+	if (multiboot_magic == 0x36d76289) {
+		kprintf("[ " TERMCOLOR_GREEN "OK" TERMCOLOR_RESET " ] Mulitboot magic: 0x%x (correct)\n", multiboot_magic);
+	} else {
+		kprintf("[" TERMCOLOR_RED "FAIL" TERMCOLOR_RESET "] Mulitboot magic: 0x%x (incorrect)\n", multiboot_magic);
+	}
 }
