12e524cf88881a112c0383c39c5c839a22949b54
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2022-02-17 13:20:09 +0000

    Add div+ceil macro

--- src/libk/include/utils.h
+++ src/libk/include/utils.h
@@ -4,4 +4,6 @@
 #define ALIGN_UP(ptr, alignment) ((typeof(ptr))(((uintptr_t)ptr + alignment - 1) & ~(alignment - 1)))
 #define ADD_BYTES(ptr, offset) ((typeof(ptr))((uintptr_t)ptr + offset))
 
+#define IDIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
+
 #endif
