df7f4fc5a7969069a8a5483c6e47211157dba2ac
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2022-03-16 20:17:51 +0000

    Add align down macro

--- src/libk/include/utils.h
+++ src/libk/include/utils.h
@@ -4,6 +4,7 @@
 #include <panic.h>
 
 #define ALIGN_UP(ptr, alignment) ((typeof(ptr))(((uintptr_t)ptr + alignment - 1) & ~(alignment - 1)))
+#define ALIGN_DOWN(ptr, alignment) ((typeof(ptr))(((uintptr_t)ptr) & ~(alignment - 1)))
 #define ADD_BYTES(ptr, offset) ((typeof(ptr))((uintptr_t)ptr + offset))
 
 #define IDIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
