5fb360784e6869a89765a13095e121bea1906f8f
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2022-03-31 22:49:20 +0200

    Add very basic scheduling functions and locking

---
+++ src/threading/threading.cpp
@@ -0,0 +1,73 @@
+#include "threading.hpp"
+
+using namespace threads;
+
+atomic_uint_fast64_t threads::next_pid = 1;
+
+Scheduler threads::scheduler = Scheduler();
+
+extern "C" void task_init(void);
+extern "C" void task_switch(Task *new_task);
+extern "C" Task* current_task_ptr = nullptr;
+
+void Scheduler::print_tasks() {
+	for (auto& task : tasks) {
+		printf("%lli: %s\n", task->get_pid(), task->get_name().c_str());
+	}
+}
+
+std::shared_ptr<Task> Task::new_kernel_task(std::string name, void(*entry_func)(uint64_t, uint64_t, uint64_t), uint64_t arg1, uint64_t arg2, uint64_t arg3) {
+	uint64_t cr3;
+	__asm__ volatile("mov %%cr3, %0" : "=r"(cr3));
+	auto task = std::make_shared<Task>(name, cr3);
+	auto& stack = task->get_stack();
+
+	*((uint64_t*)stack.top - 1) = (uint64_t)entry_func;
+	*((uint64_t*)stack.top - 2) = (uint64_t)task_init;
+	*((uint64_t*)stack.top - 3) = (uint64_t)0; // rbx
+	*((uint64_t*)stack.top - 4) = (uint64_t)0; // rbp
+	*((uint64_t*)stack.top - 5) = (uint64_t)0; // r12
+	*((uint64_t*)stack.top - 6) = (uint64_t)arg1; // r13 - task_init arg 1
+	*((uint64_t*)stack.top - 7) = (uint64_t)arg2; // r14 - task_init arg 2
+	*((uint64_t*)stack.top - 8) = (uint64_t)arg3; // r15 - task_init arg 3
+	return task;
+}
+
+std::shared_ptr<Task> threads::init_multitasking(uint64_t stack_bottom, uint64_t stack_top) {
+	uint64_t cr3;
+	__asm__ volatile("mov %%cr3, %0" : "=r"(cr3));
+	auto kernel_task = std::make_shared<threads::Task>("kernel_task", cr3, stack_top, stack_top);
+	current_task_ptr = kernel_task.get();
+	scheduler.lock().add_task(kernel_task);
+	return kernel_task;
+}
+
+void Scheduler::schedule() {
+	if (running_tasks.size() == 1) return; // Only one task - don't need to switch
+	current_task_index++;
+	if (current_task_index >= running_tasks.size()) current_task_index = 0;
+	task_switch(running_tasks[current_task_index].get());
+}
+
+void Scheduler::add_task(std::shared_ptr<Task> task) {
+	tasks.push_back(task);
+	running_tasks.push_back(task);
+}
+
+SchedulerLock Scheduler::lock() {
+	return SchedulerLock(this);
+}
+
+SchedulerLock::SchedulerLock(Scheduler *s): s(s) {
+	__asm__ volatile("cli");
+	s->IRQ_disable_counter++;
+}
+
+SchedulerLock::~SchedulerLock() {
+	s->IRQ_disable_counter--;
+	if (s->IRQ_disable_counter == 0) __asm__ volatile("sti");
+}
+
+void SchedulerLock::print_tasks() { s->print_tasks(); }
+void SchedulerLock::add_task(std::shared_ptr<Task> task) { s->add_task(task); }
+void SchedulerLock::schedule() { s->schedule(); }
--- src/threading/threading.hpp
+++ src/threading/threading.hpp
@@ -1,6 +1,80 @@
 #ifndef _HUGOS_THREADING_H
 #define _HUGOS_THREADING_H
 
+#include <vector>
+#include <memory>
+#include <string>
+#include <stdint.h>
+#include <stdatomic.h>
+#include "../memory/stack.hpp"
+#include "../memory/paging.h"
+#include "../main/main.h"
 
+namespace threads {
+	extern "C" struct Task;
+	extern atomic_uint_fast64_t next_pid;
+
+	std::shared_ptr<Task> init_multitasking(uint64_t stack_bottom, uint64_t stack_top);
+
+	enum class task_state {
+		RUNNING
+	};
+
+	extern "C" struct Task {
+		private:
+		Stack stack;
+		uint64_t stack_ptr;
+		uint64_t p4_page_table;
+		uint64_t pid;
+		std::string name;
+		task_state state;
+
+		public:
+		Task(std::string name, uint64_t p4_page_table = create_p4_table(global_frame_allocator)): pid(atomic_fetch_add(&next_pid, 1)), name(std::move(name)), state(task_state::RUNNING), stack(40*1024), p4_page_table(p4_page_table), stack_ptr(this->stack.top - 8*8 /* 8 uint64_t get popped off on entry - 6 regs + 2 return */) {}
+		Task(std::string name, uint64_t p4_page_table, uint64_t stack_top, uint64_t stack_bottom): pid(atomic_fetch_add(&next_pid, 1)), name(std::move(name)), state(task_state::RUNNING), stack(stack_top, stack_bottom), p4_page_table(p4_page_table), stack_ptr(0) {}
+
+		public:
+		uint64_t get_pid() { return pid; }
+		std::string& get_name() { return name; }
+		task_state get_state() { return state; }
+		uint64_t get_p4_page_table() { return p4_page_table; }
+		Stack& get_stack() { return stack; }
+
+		static std::shared_ptr<Task> new_kernel_task(std::string name, void(*entry_func)(uint64_t, uint64_t, uint64_t), uint64_t arg1, uint64_t arg2, uint64_t arg3);
+	};
+
+	class SchedulerLock;
+
+	class Scheduler {
+		friend class SchedulerLock;
+		private:
+		std::vector<std::shared_ptr<Task>> tasks;
+		std::vector<std::shared_ptr<Task>> running_tasks;
+		size_t current_task_index = 0;
+		int IRQ_disable_counter = 0;
+
+		void print_tasks();
+		void add_task(std::shared_ptr<Task>);
+		void schedule();
+
+		public:
+		SchedulerLock lock();
+	};
+
+	class SchedulerLock {
+		friend class Scheduler;
+		private:
+		Scheduler *s;
+		SchedulerLock(Scheduler *s);
+
+		public:
+		~SchedulerLock();
+		void print_tasks();
+		void add_task(std::shared_ptr<Task>);
+		void schedule();
+	};
+
+	extern Scheduler scheduler;
+}
 
 #endif
