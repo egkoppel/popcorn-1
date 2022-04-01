87ee82c4d7e1bd91f992fadd83a679c0c2b5cd77
Oliver Hiorns <oshiorns1@gmail.com>
2022-04-01 19:09:50 +0100

    add standard mechanism to disable asserts
    defining NDEBUG before including assert.h will cause all asserts to
    expand to empty statements

--- src/libk/include/assert.h
+++ src/libk/include/assert.h
@@ -3,8 +3,13 @@
 
 #include <panic.h>
 
+#ifdef NDEBUG
+#define assert(x) {}
+#define assert_msg(x, s, ...) {}
+#else
 #define assert(x) {if (!(x)) panic("Assertion failed: " #x "\n");}
 #define assert_msg(x, s, ...) {if (!(x)) panic("Assertion failed: " #x "\n" s, ##__VA_ARGS__);}
+#endif
 
 #ifndef __cplusplus
 #define STATIC_ASSERT(cond)  typedef char __static_assert_type[ (cond) ? 1 : -1 ];
