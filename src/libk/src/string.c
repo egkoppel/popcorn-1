84e5309c2c575cc3bdb8c84e160077aa4dfd2c3c
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2022-10-18 23:20:00 +0100

    Move string.c functions into userspace available section so fsd/uinit not broken

--- src/libk/src/string.c
+++ src/libk/src/string.c
@@ -1,6 +1,16 @@
+/*
+ * Copyright (c) 2022 Eliyahu Gluschove-Koppel.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stddef.h>
 
-void *memcpy(void *dest, const void *src, size_t num) {
+__attribute__((section(".userspace.text"))) void *memcpy(void *dest, const void *src, size_t num) {
 	for (size_t i = 0; i < num; ++i) {
 		((char *)dest)[i] = ((char *)src)[i];
 	}
@@ -8,7 +18,7 @@ void *memcpy(void *dest, const void *src, size_t num) {
 	return dest;
 }
 
-void *memmove(void *dest, const void *src, size_t num) {
+__attribute__((section(".userspace.text"))) void *memmove(void *dest, const void *src, size_t num) {
 	if (dest < src) { // copy left to right
 		for (size_t i = 0; i < num; ++i) {
 			((char *)dest)[i] = ((char *)src)[i];
@@ -22,19 +32,19 @@ void *memmove(void *dest, const void *src, size_t num) {
 	return dest;
 }
 
-void *memset(void *ptr, int value, size_t n) {
+__attribute__((section(".userspace.text"))) void *memset(void *ptr, int value, size_t n) {
 	for (size_t i = 0; i < n; ++i)
 		((unsigned char *)ptr)[i] = (unsigned char)value;
 	return ptr;
 }
 
-size_t strlen(const char *str) {
+__attribute__((section(".userspace.text"))) size_t strlen(const char *str) {
 	size_t len = -1;
 	while (str[++len] != '\0');
 	return len;
 }
 
-int memcmp(const void *ptr1, const void *ptr2, size_t count) {
+__attribute__((section(".userspace.text"))) int memcmp(const void *ptr1, const void *ptr2, size_t count) {
 	const char *s1 = ptr1;
 	const char *s2 = ptr2;
 	while (count-- > 0) {
@@ -44,7 +54,7 @@ int memcmp(const void *ptr1, const void *ptr2, size_t count) {
 	return 0;
 }
 
-int strcmp(const char *s1, const char *s2) {
+__attribute__((section(".userspace.text"))) int strcmp(const char *s1, const char *s2) {
 	if (s1 == NULL || s2 == NULL) {
 		if (s1 < s2) return -1;
 		if (s1 > s2) return 1;
@@ -64,7 +74,7 @@ int strcmp(const char *s1, const char *s2) {
 	}
 }
 
-int strncmp(const char *s1, const char *s2, size_t n) {
+__attribute__((section(".userspace.text"))) int strncmp(const char *s1, const char *s2, size_t n) {
 	if (s1 == NULL || s2 == NULL) {
 		if (s1 < s2) return -1;
 		if (s1 > s2) return 1;
