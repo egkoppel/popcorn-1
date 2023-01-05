f57c2f3e70b7d0e1ddc64136158005c805f89c9b
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-01-05 20:37:28 +0000

    Implement task creation

--- kernel/src/threading/task.cpp
+++ kernel/src/threading/task.cpp
@@ -11,12 +11,49 @@
 
 #include "task.hpp"
 
+#include <arch/threading.hpp>
+
+using namespace memory;
+
 namespace threads {
 	atomic_uint_fast64_t Task::next_pid = 1;
 
-	Task::Task(memory::KStack<>&& stack) : stack(std::move(stack)), address_space_(), stack_ptr(0_va) {}
+	Task::Task(const char *name, memory::KStack<>&& stack)
+		: stack(std::move(stack)),
+		  address_space_(),
+		  stack_ptr_(0_va),
+		  name(name) {}
 
 	std::unique_ptr<Task> Task::initialise(memory::KStack<>&& current_stack) {
-		return std::unique_ptr<Task>(new Task{std::move(current_stack)});
+		auto ktask = new Task{"kmain", std::move(current_stack)};
+		return std::unique_ptr<Task>(ktask);
+	}
+
+	Task::Task(const char *name, void (*entrypoint)(usize), usize argument)
+		: stack(memory::constants::frame_size),
+		  address_space_(),
+		  stack_ptr_(vaddr_t(*this->stack.top()) - 8 * 8),
+		  name(name) {
+		auto stack_top = static_cast<u64 *>((*this->stack.top()).address);
+		stack_top[-1]  = reinterpret_cast<u64>(entrypoint);
+		stack_top[-2]  = reinterpret_cast<u64>(arch::task_startup);
+		stack_top[-3]  = argument;
+		stack_top[-4]  = 0;
+		stack_top[-5]  = 0;
+		stack_top[-6]  = 0;
+		stack_top[-7]  = 0;
+		stack_top[-8]  = 0;
+	}
+
+	usize get_p4_table_frame(const Task *task) {
+		auto ret = task->address_space().l4_table_frame()->addr();
+		LOG(Log::DEBUG, "get_p4_table_frame() -> %lp", ret);
+		return ret;
+	}
+	usize *task_stack_ptr_storage(Task *task) {
+		return &task->stack_ptr().address;
+	}
+	usize get_kstack_top(const Task *task) {
+		return (*task->kernel_stack().top()).address.address;
 	}
 }   // namespace threads
--- kernel/src/threading/task.hpp
+++ kernel/src/threading/task.hpp
@@ -27,11 +27,12 @@ namespace threads {
 
 		memory::KStack<> stack;
 		memory::paging::AddressSpace address_space_;
-		memory::vaddr_t stack_ptr;
-		uint64_t pid              = atomic_fetch_add(&next_pid, 1);
-		State state               = State::RUNNING;
+		memory::vaddr_t stack_ptr_;
+		uint64_t pid = atomic_fetch_add(&next_pid, 1);
+		State state  = State::RUNNING;
+		const char *name;
 
-		explicit Task(memory::KStack<>&& stack);
+		explicit Task(const char *name, memory::KStack<>&& stack);
 
 	public:
 		Task()            = delete;
@@ -39,6 +40,7 @@ namespace threads {
 		Task(const Task& other, deep_copy_t) : stack(other.stack, deep_copy) {}
 		Task(Task&&) = default;
 		~Task()      = default;
+		Task(const char *name, void (*entrypoint)(usize), usize argument);
 
 		static std::unique_ptr<Task> initialise(memory::KStack<>&& current_stack);
 
@@ -46,7 +48,14 @@ namespace threads {
 		inline State get_state() const { return this->state; }
 		inline memory::paging::AddressSpace& address_space() { return this->address_space_; }
 		inline const memory::paging::AddressSpace& address_space() const { return this->address_space_; }
+		memory::vaddr_t& stack_ptr() { return this->stack_ptr_; }
+		const memory::vaddr_t& stack_ptr() const { return this->stack_ptr_; }
+		const memory::KStack<>& kernel_stack() const { return this->stack; }
 	};
+
+	extern "C" usize get_p4_table_frame(const Task *);
+	extern "C" usize *task_stack_ptr_storage(Task *);
+	extern "C" usize get_kstack_top(const Task *);
 }   // namespace threads
 
 #endif   // HUGOS_KERNEL_SRC_THREADING_TASK_HPP
