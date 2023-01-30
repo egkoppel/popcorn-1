5c87c4dad188c87290ba39fb5e86a9049b1a9cb5
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-01-30 20:57:16 +0000

    Build drivers properly into the ramdisk

--- cmake/RamdiskGenerate.cmake
+++ cmake/RamdiskGenerate.cmake
@@ -1,8 +1,17 @@
 find_program(TAR NAMES gtar tar)
 
+set(RAMDISK_GENERATE_COMMAND mkdir initramfs)
+
+foreach (DRIVER ${RAMDISK_INCLUDED_DRIVERS})
+    set(RAMDISK_GENERATE_COMMAND ${RAMDISK_GENERATE_COMMAND} && cp $<TARGET_FILE:${DRIVER}> initramfs)
+endforeach ()
+
 add_custom_command(OUTPUT initramfs
-        COMMAND mkdir initramfs)
+        DEPENDS ${RAMDISK_INCLUDED_DRIVERS}
+        COMMAND ${RAMDISK_GENERATE_COMMAND})
 
 add_custom_command(OUTPUT ramdisk.tar.gz
-        DEPENDS initramfs
+        DEPENDS initramfs ${RAMDISK_INCLUDED_DRIVERS}
         COMMAND ${TAR} -czf ramdisk.tar.gz initramfs)
+
+list(APPEND ADDITIONAL_CLEAN_FILES "initramfs" "ramdisk.tar.gz")
