ee5868370a079a1bca9a5884faef9a34b83dfb51
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-02-01 10:27:17 +0000

    Add monotonic allocator placement allocate implementation

--- kernel/src/memory/virtual_allocators/monotonic_allocator.cpp
+++ kernel/src/memory/virtual_allocators/monotonic_allocator.cpp
@@ -23,4 +23,19 @@ namespace memory::virtual_allocators {
 			return start;
 		}
 	}
+
+	aligned<vaddr_t> MonotonicAllocator::allocate_at_(aligned<vaddr_t> start, uint64_t byte_length) {
+		LOG(Log::DEBUG, "Monotonic virtual allocator currently at %lp", this->current_);
+		if (start >= this->current_) {
+			auto end = start + IDIV_ROUND_UP(byte_length, constants::frame_size);
+			if (end > this->end_) {
+				THROW(std::bad_alloc());
+			} else {
+				this->current_ = end;
+				return start;
+			}
+		} else {
+			THROW(std::bad_alloc());
+		}
+	}
 }   // namespace memory::virtual_allocators
