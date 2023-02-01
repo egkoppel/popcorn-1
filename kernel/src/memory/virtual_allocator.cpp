0a255248db76caf28e466f429e6bc0dcef41eea9
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2023-02-01 10:21:18 +0000

    Add virtual allocator allocate debug prints

--- kernel/src/memory/virtual_allocator.cpp
+++ kernel/src/memory/virtual_allocator.cpp
@@ -12,9 +12,11 @@
 
 namespace memory {
 	aligned<vaddr_t> IVirtualAllocator::allocate(uint64_t byte_length) {
+		LOG(Log::DEBUG, "Virtual allocation requested of size %lx", byte_length);
 		return this->allocate_(byte_length);
 	}
 	aligned<vaddr_t> IVirtualAllocator::allocate(aligned<vaddr_t> hint, u64 byte_length) {
+		LOG(Log::DEBUG, "Physical allocation requested of size %lx at %lp", byte_length, hint);
 		return this->allocate_at_(hint, byte_length);
 	}
 
